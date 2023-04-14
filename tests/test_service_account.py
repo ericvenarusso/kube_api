d77b4e847f565b912424ba32ad27c5c98d398627
root <root@DESKTOP-BI64J76>
2023-04-13 22:40:53 -0300

    Fix test name

--- tests/test_service_account.py
+++ tests/test_service_account.py
@@ -72,7 +72,7 @@ class TestServiceAccount:
             ]
         ),
     )
-    def test_service_account_nonexistence(self, _):
+    def test_service_account_existence(self, _):
         service_account = ServiceAccount(name="test")
         assert service_account.exists() == True
 
@@ -86,6 +86,6 @@ class TestServiceAccount:
             ]
         ),
     )
-    def test_service_account_existence(self, _):
+    def test_service_account_nonexistence(self, _):
         service_account = ServiceAccount(name="test")
         assert service_account.exists() == False
